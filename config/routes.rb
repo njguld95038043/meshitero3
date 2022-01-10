6e239ce88b0d1b0ecdc31cb86b4963c5c6682530
EC2 Default User <ec2-user@ip-172-31-22-154.ec2.internal>
2022-01-10 00:31:16 +0000

    [Finish]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as:'about'
   devise_for :users
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   root to:"homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
