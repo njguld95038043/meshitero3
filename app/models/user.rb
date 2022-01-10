5a6a7ce22d760b972fde7945c4d1cfb2f239da80
EC2 Default User <ec2-user@ip-172-31-22-154.ec2.internal>
2022-01-10 06:28:33 +0000

    [Finish]4-15

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   has_many :post_images, dependent: :destroy
   has_one_attached :profile_image
 
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/jpeg')
-    end
-      profile_image.variant(resize: size).processed
+  def get_profile_image(size)
+   unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+   end
+  profile_image.variant(resize: size).processed
   end
 end
